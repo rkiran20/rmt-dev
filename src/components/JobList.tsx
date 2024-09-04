63b25f4f1d06ea934d1f81f8bca9b72ef66b3010
vavilapalli20 <119149464+vavilapalli20@users.noreply.github.com>
2024-09-04 22:39:00 +0530

    cleared bug

---
+++ src/components/JobList.tsx
@@ -0,0 +1,30 @@
+import { useActiveId } from "../lib/hooks";
+import { JobItem } from "../lib/types";
+import JobListItem from "./JobListItem";
+import Spinner from "./Spinner";
+
+type JobListProps = {
+  jobItems: JobItem[];
+  isLoading: boolean;
+};
+
+export function JobList({ jobItems, isLoading }: JobListProps) {
+  const activeId = useActiveId();
+
+  return (
+    <ul className="job-list">
+      {isLoading && <Spinner />}
+
+      {!isLoading &&
+        jobItems.map((jobItem) => (
+          <JobListItem
+            key={jobItem.id}
+            jobItem={jobItem}
+            isActive={jobItem.id === activeId}
+          />
+        ))}
+    </ul>
+  );
+}
+
+export default JobList;
